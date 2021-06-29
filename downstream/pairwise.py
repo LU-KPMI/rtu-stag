2f8d3311ce0f8324af386fa86eeccf4ee2eefb4d
Kristaps Čivkulis <kristaps.civkulis@gmail.com>
2021-06-29 19:09:13 +0300

    Add dendrograms

--- downstream/pairwise.py
+++ downstream/pairwise.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.spatial import distance
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 
 def bray_curtis(u, v):
@@ -27,6 +28,8 @@ for treatment in ["STD2", "STD3", "Control"]:
         after = np.array(data.loc[sample_name_after])
         sample_abundances.append([patient_id, treatment, before, after])
 
+
+
 n = len(sample_abundances)
 
 d_before, d_after = np.zeros(shape=(n, n)), np.zeros(shape=(n, n))
@@ -36,40 +39,40 @@ for i in range(n):
         d_before[i][j] = distance.jensenshannon(sample_abundances[i][2], sample_abundances[j][2])
         d_after[i][j]  = distance.jensenshannon(sample_abundances[i][3], sample_abundances[j][3])
 
-avg_d_before = {}
-avg_d_after = {}
+#avg_d_before = {}
+#avg_d_after = {}
+#
+#for i in range(n):
+#    for j in range(i):
+#        if sample_abundances[i][1] == sample_abundances[j][1]:
+#            t = sample_abundances[i][1]
+#            if t not in avg_d_before:
+#                avg_d_before[t] = 0
+#                avg_d_after[t] = 0
+#            avg_d_before[t] += d_before[i][j]
+#            avg_d_after[t] += d_after[i][j]
+#
+#for t in avg_d_before:
+#    c = 0
+#    for s in sample_abundances:
+#        if s[1] == t:
+#            c += 1
+#    avg_d_before[t] /= c * (c - 1) / 2
+#    avg_d_after[t] /= c * (c - 1) / 2
+#
+#
+#print('avg_d_before =', avg_d_before)
+#print('avg_d_after =', avg_d_after)
 
-for i in range(n):
-    for j in range(i):
-        if sample_abundances[i][1] == sample_abundances[j][1]:
-            t = sample_abundances[i][1]
-            if t not in avg_d_before:
-                avg_d_before[t] = 0
-                avg_d_after[t] = 0
-            avg_d_before[t] += d_before[i][j]
-            avg_d_after[t] += d_after[i][j]
+fig, axs = plt.subplots(2, 1, figsize=(7, 14))
 
-for t in avg_d_before:
-    c = 0
-    for s in sample_abundances:
-        if s[1] == t:
-            c += 1
-    avg_d_before[t] /= c * (c - 1) / 2
-    avg_d_after[t] /= c * (c - 1) / 2
-
-
-print('avg_d_before =', avg_d_before)
-print('avg_d_after =', avg_d_after)
-
-fig, ax = plt.subplots(2, 1, figsize=(7, 14))
-
-g1 = sns.heatmap(d_before, linewidth = 0.5, ax=ax[0], square=True)
+g1 = sns.heatmap(d_before, linewidth = 0.5, ax=axs[0], square=True)
 g1.set(xlabel = None, ylabel = None)
 g1.set(xticklabels = [], yticklabels = [])
 g1.tick_params(left=False, bottom=False)
 g1.set(title="Before")
 
-g2 = sns.heatmap(d_after, linewidth = 0.5, ax=ax[1], square=True)
+g2 = sns.heatmap(d_after, linewidth = 0.5, ax=axs[1], square=True)
 g2.set(xlabel = None, ylabel = None)
 g2.set(xticklabels = [], yticklabels = [])
 g2.tick_params(left=False, bottom=False)
@@ -77,3 +80,15 @@ g2.set(title="After")
 
 plt.tight_layout()
 plt.savefig('heatmap.png')
+
+
+fig, axs = plt.subplots(2, 1, figsize=(11, 6))
+linkage_matrix_before = linkage(distance.squareform(d_before), "single")
+linkage_matrix_after = linkage(distance.squareform(d_after), "single")
+dendrogram(linkage_matrix_before, labels=list(zip(*sample_abundances))[1], ax=axs[0])
+dendrogram(linkage_matrix_after, labels=list(zip(*sample_abundances))[1], ax=axs[1])
+axs[0].set_title("before")
+axs[1].set_title("after")
+plt.tight_layout()
+plt.savefig("dendrograms.svg")
+
