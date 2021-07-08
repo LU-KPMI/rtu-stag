3fefdf31b1b020ac6aa92c22f60d9586240481d8
Kristaps Čivkulis <kristaps.civkulis@gmail.com>
2021-07-08 16:01:40 +0300

    Restructure analysis

--- downstream/process.sh
+++ downstream/process.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-python3 ./extract.py
+python3 extract.py
 echo "Setup done"
 
-python3 ./generate_abundance_table.py > abundances.csv
-echo "Abundance table generated"
-
-python3 ./calc_richness.py > richness.csv
-echo "Richness calculated"
-
-python3 ./rarefaction.py
-echo "Rarefaction generated"
-
-python3 ./graphs.py
-echo "Richness summary visualization complete"
-
-python3 ./pairwise.py
-echo "Pairwise analysis done"
-
-python3 ./enterotypes.py
-echo "Basic enterotype analysis done"
+python3 analysis.py
+echo "Analysis done"
